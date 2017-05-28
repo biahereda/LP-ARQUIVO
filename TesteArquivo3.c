a3e53829bfe652c98b943984484acdccfbb398fb
Bianca <heredabianca@gmail.com>
2017-05-28 16:00:02 -0300

    Add files via upload

---
+++ TesteArquivo3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(void)
+{
+  FILE *pont_arq, *pont;
+  char texto_str[20];
+
+  pont_arq = fopen("arquivo_palavra.txt", "r");
+
+
+  while(fgets(texto_str, 20, pont_arq) != NULL)
+    printf("%s", texto_str);
+
+
+  fclose(pont_arq);
+
+  getch();
+  return(0);
+}
